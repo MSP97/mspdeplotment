be76a230efb37df07b41e1fa247de0b6fa067cf1
MSP97 <marishunmugapandian@gmail.com>
2026-01-11 22:38:50 +0530

    2nd changes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d3d45dcf251823c1769909cd43698d126db38d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2633585290"
+    serviceWorkerVersion: "638030743"
   }
 });
